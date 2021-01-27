337916da98a70f5a01e54876b696ff09eae6e9b8
Dejan Milicic <dejan.milicic@gmail.com>
2021-01-27 22:05:33 +0100

    Add healthcheck and RavenDB

---
+++ src/Digitalis/Settings.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Digitalis
+{
+    public class Settings
+    {
+        public DatabaseSettings Database { get; set; }
+
+        public class DatabaseSettings
+        {
+            public string[] Urls { get; set; }
+
+            public string DatabaseName { get; set; }
+
+            public string CertPath { get; set; }
+
+            public string CertPass { get; set; }
+        }
+    }
+}
--- src/Digitalis/Startup.cs
+++ src/Digitalis/Startup.cs
@@ -9,7 +9,11 @@ using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
+using Raven.Client.Documents;
+using Raven.Client.Documents.Indexes;
+using Raven.Client.Documents.Session;
 
 namespace Digitalis
 {
@@ -25,12 +29,35 @@ namespace Digitalis
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddHealthChecks();
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Digitalis", Version = "v1" });
             });
+
+            services.AddSingleton<IDocumentStore>(_ =>
+            {
+                var dbConfig = Configuration.GetSection("Database").Get<Settings.DatabaseSettings>();
+
+                var store = new DocumentStore
+                {
+                    Urls = dbConfig.Urls,
+                    Database = dbConfig.DatabaseName
+                };
+
+                if (!string.IsNullOrWhiteSpace(dbConfig.CertPath))
+                    store.Certificate = new X509Certificate2(dbConfig.CertPath, dbConfig.CertPass);
+
+                store.Initialize();
+
+                IndexCreation.CreateIndexes(typeof(Startup).Assembly, store);
+
+                return store;
+            });
+
+            services.AddScoped<IAsyncDocumentSession>(sp => sp.GetService<IDocumentStore>()?.OpenAsyncSession());
+            services.AddScoped<IDocumentSession>(sp => sp.GetService<IDocumentStore>()?.OpenSession());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -49,6 +76,7 @@ namespace Digitalis
 
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapHealthChecks("/healthcheck");
                 endpoints.MapControllers();
             });
         }
