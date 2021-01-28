c75645b447a95fd713088d36be60c047e68a96ae
Dejan Milicic <dejan.milicic@gmail.com>
2021-01-28 11:08:07 +0100

    security wip

--- src/Specs/Infrastructure/Fixture.cs
+++ src/Specs/Infrastructure/Fixture.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Security.Claims;
@@ -6,6 +7,8 @@ using System.Text.Encodings.Web;
 using System.Threading.Tasks;
 using Digitalis;
 using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.AspNetCore.TestHost;
 using Microsoft.Extensions.DependencyInjection;
@@ -28,19 +31,27 @@ namespace Specs.Infrastructure
             Store = this.GetDocumentStore();
             IndexCreation.CreateIndexes(typeof(Startup).Assembly, Store);
 
-            HttpClient = factory
-                .CreateClientWithTestAuth()
-                ;
+            //HttpClient = factory
+            //    .CreateClientWithTestAuth()
+            //    ;
+
+            HttpClient = factory.WithWebHostBuilder(builder => builder.ConfigureTestServices(
+                services => services.AddMvc(
+                    options =>
+                    {
+                        options.Filters.Add(new AllowAnonymousFilter());
+                        options.Filters.Add(new FakeUserFilter());
+                    }))).CreateClient();
 
 
-//HttpClient = factory.WithWebHostBuilder(builder =>
-//    {
-//        builder.ConfigureTestServices(services =>
-//        {
-//            services.AddSingleton<IDocumentStore>(Store);
-//        });
-//    })
-//    .CreateDefaultClient();
+            //HttpClient = factory.WithWebHostBuilder(builder =>
+            //    {
+            //        builder.ConfigureTestServices(services =>
+            //        {
+            //            services.AddSingleton<IDocumentStore>(Store);
+            //        });
+            //    })
+            //    .CreateDefaultClient();
 
 
             //HttpClient = factory.WithAuthentication()
@@ -110,6 +121,25 @@ namespace Specs.Infrastructure
             var result = AuthenticateResult.Success(ticket);
 
             return Task.FromResult(result);
+        }
+    }
+
+    public class FakeUserFilter : IAsyncActionFilter
+    {
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            context.HttpContext.User = new ClaimsPrincipal(
+                new ClaimsIdentity(new List<Claim>
+                {
+                    new Claim(ClaimTypes.NameIdentifier, "12345678-1234-1234-1234-123456789012"),
+                    new Claim(ClaimTypes.Name, "TestUser"),
+                    new Claim(ClaimTypes.Email, "test.user@example.com"), // add as many claims as you need
+                }
+                , "TestAuth"
+                )
+            );
+
+            await next();
         }
     }
 }
