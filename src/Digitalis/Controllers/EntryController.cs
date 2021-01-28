e1ff4c86a1fb2e2ec4555a67c93642b95e9aefde
Dejan Milicic <dejan.milicic@gmail.com>
2021-01-28 18:28:31 +0100

    code examples

--- src/Digitalis/Controllers/EntryController.cs
+++ src/Digitalis/Controllers/EntryController.cs
@@ -8,6 +8,10 @@ using Digitalis.Infrastructure;
 using Digitalis.Models;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using NSwag.Annotations;
+using NSwag.Generation.Processors;
+using NSwag.Generation.Processors.Contexts;
 using Raven.Client.Documents;
 using Raven.Client.Documents.BulkInsert;
 using Raven.Client.Documents.Operations;
@@ -68,7 +72,58 @@ namespace Digitalis.Controllers
         [ProducesResponseType(403)]
         [ProducesResponseType(500)]
         [Produces(typeof(string))]
+        [ReDocCodeSample("json", "{ tags: \"sky\"}")]
+        [ReDocCodeSample("c#", "console.log()")]
         [HttpPost("entry")]
         public async Task<string> Post([FromBody] CreateEntry.Command entry) => await _mediator.Send(entry);
+    }
+
+    public class ReDocCodeSampleAttribute : OpenApiOperationProcessorAttribute
+    {
+        public ReDocCodeSampleAttribute(string language, string source)
+            : base(typeof(ReDocCodeSampleAppender), language, source)
+        {
+        }
+    }
+
+    public class ReDocCodeSampleAppender : IOperationProcessor
+    {
+        private readonly string _language;
+        private readonly string _source;
+        private const string ExtensionKey = "x-code-samples";
+
+        public ReDocCodeSampleAppender(string language, string source)
+        {
+            _language = language;
+            _source = source;
+        }
+
+        public bool Process(OperationProcessorContext context)
+        {
+            if (context.OperationDescription.Operation.ExtensionData == null)
+                context.OperationDescription.Operation.ExtensionData = new Dictionary<string, object>();
+
+            var data = context.OperationDescription.Operation.ExtensionData;
+            if (!data.ContainsKey(ExtensionKey))
+                data[ExtensionKey] = new List<ReDocCodeSample>();
+
+            var samples = (List<ReDocCodeSample>)data[ExtensionKey];
+            samples.Add(new ReDocCodeSample
+            {
+                Language = _language,
+                Source = _source,
+            });
+
+            return true;
+        }
+    }
+
+    internal class ReDocCodeSample
+    {
+        [JsonProperty("lang")]
+        public string Language { get; set; }
+
+        [JsonProperty("source")]
+        public string Source { get; set; }
     }
 }
