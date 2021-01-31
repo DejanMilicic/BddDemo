f2fc4cad0d8d2210120a2d283495cdc23ac252c5
Dejan Milicic <dejan.milicic@gmail.com>
2021-01-31 13:39:34 +0100

    intentional bug intentionally removed

--- src/Specs/Features/AddNewEntry/HappyPath.cs
+++ src/Specs/Features/AddNewEntry/HappyPath.cs
@@ -49,7 +49,7 @@ namespace Specs.Features.AddNewEntry
         {
             Store.OpenSession().Query<Entry>().Statistics(out QueryStatistics stats).ToList();
 
-            stats.TotalResults.Should().Be(0);
+            stats.TotalResults.Should().Be(1);
         }
 
         [Fact(DisplayName = "3. New entry has expected content")]
