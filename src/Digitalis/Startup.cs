9cd478117d1e53b422946c87153d533b5c8e8251
Dejan Milicic <dejan.milicic@gmail.com>
2021-02-02 17:58:13 +0100

    this is API, not needed

--- src/Digitalis/Startup.cs
+++ src/Digitalis/Startup.cs
@@ -72,13 +72,7 @@ namespace Digitalis
             services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidatorPipelineBehavior<,>));
 
             services.AddAuthorization();
-            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
-                .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme,
-                    options =>
-                    {
-                        options.LoginPath = new PathString("/auth/login");
-                        options.AccessDeniedPath = new PathString("/auth/denied");
-                    });
+            services.AddAuthentication();
 
             services.Scan(
                 x =>
