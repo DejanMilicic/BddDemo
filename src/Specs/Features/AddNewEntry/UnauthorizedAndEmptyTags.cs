b2ccac0ae4d1472d7fb9038f3ae859991e253fd0
Dejan Milicic <dejan.milicic@gmail.com>
2021-02-03 01:39:05 +0100

    testing the order of pipeline checks - auth before validation

---
+++ src/Specs/Features/AddNewEntry/UnauthorizedAndEmptyTags.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Digitalis;
+using Digitalis.Features;
+using Digitalis.Infrastructure;
+using Digitalis.Models;
+using FakeItEasy;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Raven.Client.Documents.Session;
+using Specs.Infrastructure;
+using Xunit;
+
+namespace Specs.Features.AddNewEntry
+{
+    [Trait("Add New Entry", "Unauthorized and Empty Tags")]
+    public class UnauthorizedAndEmptyTags : Fixture
+    {
+        private readonly HttpResponseMessage _response;
+        private readonly CreateEntry.Command _newEntry;
+
+        public UnauthorizedAndEmptyTags(WebApplicationFactory<Startup> factory) : base(factory)
+        {
+            var client = AuthClient(new Dictionary<string, string>
+            {
+                { "email", "john@doe.com" }
+            });
+
+            _newEntry = new CreateEntry.Command(new string[] { });
+
+            _response = client.PostAsync("/entry",
+                Serialize(_newEntry)).Result;
+
+            WaitForIndexing(Store);
+            WaitForUserToContinueTheTest(Store);
+        }
+
+        [Fact(DisplayName = "1. Forbidden is returned")]
+        public void BadRequest()
+        {
+            _response.StatusCode.Should().Be(HttpStatusCode.Forbidden);
+        }
+
+        [Fact(DisplayName = "2. Zero entries is created in the database")]
+        public void ZeroEntryCreated()
+        {
+            Store.OpenSession().Query<Entry>().Statistics(out QueryStatistics stats).ToList();
+
+            stats.TotalResults.Should().Be(0);
+        }
+
+        [Fact(DisplayName = "3. Email is not sent")]
+        public void EmailNotSent()
+        {
+            A.CallTo(() => Mailer
+                    .SendMail(
+                        A<string>.Ignored,
+                        A<string>.Ignored,
+                        A<string>.Ignored))
+                .MustNotHaveHappened();
+        }
+    }
+}
